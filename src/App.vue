f0d816e6f00f93a199fb42d45fabe853371c87ce
liucong <liucong@metrodata.com>
2018-09-01 22:14:08 +0800

    add sass mixins

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="my-app" class="page-wrapper">
+  <div id="my-app" class="yellow">
     <!-- Loader -->
     <transition name="loader-animation" enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
       <div class="progress loader" v-if="showLoader">
@@ -7,11 +7,17 @@
       </div>
     </transition>
 
-    <transition name="page-transition" mode="out-in" appear>
-      <div class="page-content-wrapper">
-        <router-view></router-view>
-      </div>
-    </transition>
+    <article>
+      <header class="vh-100 bg-black dt w-100">
+        <div class="dtc v-mid cover ph3 ph4-m ph5-l bg-img">
+          <h1 class="f2 f-subheadline-l measure lh-title fw9">Drowning Pool Productions</h1>
+          <div class="flex ">
+            <div class="f6 fw6 ttu mr4 link-button">watch reel</div>
+            <div class="f6 fw6 ttu mr4 link-button">enter site</div>
+          </div>
+        </div>
+      </header>
+    </article>
   </div>
 </template>
 
@@ -49,4 +55,9 @@ export default {
 }
 </script>
 <style lang="scss">
+@import './styles/app.scss';
+
+.bg-img{
+  @include bg-img('https://drowningpool.tv/wp-content/uploads/2018/08/head2.jpg');
+}
 </style>
