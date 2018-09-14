088060e49b9f8d1efda50b281b9d30b403e8ea5b
liucong <liucong@metrodata.com>
2018-09-14 18:19:13 +0800

    hover trigger menu open

--- src/components/widgets/SideMenu.vue
+++ src/components/widgets/SideMenu.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="menu-container">
+  <div class="menu-container" @mouseover="changeOpenMenu(null)">
     <ul v-for="(categories, key) in list" :key="key" class="menu-ul">
-      <div class="menu-category-name" @click="changeOpenMenu(categories.name)">{{categories.name}}</div>
+      <div class="menu-category-name" @mouseover.stop="changeOpenMenu(categories.name)" @mouseout="mouseOut">{{categories.name}}</div>
       <!-- <div :class="{'menu-open': categories.name == openMenu, 'menu-close': categories.name !== openMenu}"> -->
-      <div :ref="categories.name+'_subMenu'" class="sub-menu">
+      <div @mouseover.stop="changeOpenMenu(categories.name)" :ref="categories.name+'_subMenu'" class="sub-menu">
         <router-link tag='li' class="link-button list f5 pl4 pv1" v-for="(post, key) in categories.content" :key="key" :to='"/site/" + post.id + "/" +post.slug'>{{post.title.rendered}}</router-link>
       </div>
     </ul>
@@ -41,6 +41,9 @@ export default {
 
       // console.log(subMenu)
       // console.log(this.list)
+    },
+    mouseOut(){
+      console.log('out!')
     }
   }
 }
@@ -56,7 +59,7 @@ export default {
   overflow: hidden;
   // opacity: 0;
   max-height: 0;
-  transition: max-height .2s ease-in-out;
+  transition: max-height .4s ease-in-out;
   // transition: opacity 1s ease;
 }
 
